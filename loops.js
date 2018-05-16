be0792f81b7259b9df2b96e894dda3b532275e09
Dina Hafez <dinahafez.8@gmail.com>
2018-05-16 21:00:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,9 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++) {
     if(i === 1) {
-      array.push("I am 1 strange loop.");
-      return array;
+      return "I am 1 strange loop.";
     } else {
-      array.push(`I am ${i} strange loops.`);
+      return `I am ${i} strange loops.`;
       return array;
     }
   }
