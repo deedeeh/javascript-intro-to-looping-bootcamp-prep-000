65ff68530155d2930f2a1c612192357efd7f700c
Dina Hafez <dinahafez.8@gmail.com>
2018-05-16 20:56:02 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(arr) {
+  for(var i = 0; i < 25; i++) {
+    if(i === 1) {
+      arr.push("I am 1 strange loop.");
+      return arr;
+    } else {
+      arr.push(`I am ${i} strange loops.`);
+      return arr;
+    }
+  }
+}
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(n);
+    n--;
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5; 
+}
+
+function doWhileLoop(array) {
+  do {
+    console.log(array);
+  } while(array.length > 0 && maybeTrue());
+  return array;
+}
+
